7af7a892bdf57c1c7cee39c508551019ad91d23f
Maihrendson <mcauaacademico@gmail.com>
2025-09-13 01:45:04 -0300

    feat- :arrow_up: Cria tabelas no Banco de Dados

--- myProject/app/models.py
+++ myProject/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+        #Cria número da Tabela usuário
+    number = models.CharField(max_length=13, primary_key=True, default= '')
+        #Cria nome da Tabela usuário
+    name = models.CharField(max_length=255, default= '')
+        #Cria email da Tabela usuário
+    email = models.CharField(max_length=100, default= '')
+
+    #Define como o objeto será representado como string no Django
+    def __str__(self):
+        return f"Nome: {self.name} | Email: {self.email} | Número: {self.number}"
+
