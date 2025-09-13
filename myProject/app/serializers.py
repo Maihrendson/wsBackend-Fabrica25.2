f79434044be3847c0b9d7e11e470f530e189a53a
Maihrendson <mcauaacademico@gmail.com>
2025-09-13 12:05:00 -0300

    refactor- :arrow_up: Correção de estrutura

--- myProject/app/serializers.py
+++ myProject/app/serializers.py
@@ -9,6 +9,7 @@ class AgendamentoSerializer(serializers.ModelSerializer):
 
     # Formato de entrada para a data
     date = serializers.DateTimeField(input_formats=['%d/%m/%Y %H:%M', '%Y-%m-%dT%H:%M:%S'])
+    
     class Meta:
         model = Agendamento
         fields = ['user', 'date', 'service']
